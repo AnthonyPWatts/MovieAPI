3960a70cae38d71d7af12cdd1dd88a51bd7928e6
Anthony Watts <anthonypwatts@gmail.com>
2023-03-16 12:25:37 +0000

    Commit to include unsaved files

--- src/Movies/Movies.Api/Mapping/ContractMapping.cs
+++ src/Movies/Movies.Api/Mapping/ContractMapping.cs
@@ -1,6 +1,18 @@
-﻿namespace Movies.Api.Mapping
+﻿using Movies.Application.Models;
+using Movies.Contracts.Requests;
+
+namespace Movies.Api.Mapping;
+
+public static class ContractMapping
 {
-    public class ContractMapping
+    public static Movie MapToMovie (this CreateMovieRequest request)
     {
+        return new Movie()
+        {
+            Id = Guid.NewGuid(),
+            Title = request.Title,
+            YearOfRelease = request.YearOfRelease,
+            Genres = request.Genres.ToList()
+        };
     }
 }
